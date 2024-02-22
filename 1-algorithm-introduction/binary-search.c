c1a66cb5f36ba3d9daea42da76ab303281979072
Lucas Cunegundes <lucas.cunegundes@youtan.com.br>
2024-02-22 12:55:23 -0300

    add: binary search algorithm

---
+++ 1-algorithm-introduction/binary-search.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/* 
+   Binary Search 
+   An algorithm that returns the position of the target element in a sorted array(list)
+   if the element is not found, it returns None 
+*/
+
+
+int binarySearch(int array[], int array_length, int num) {
+  
+  int left = 0;
+  int right = array_length - 1;
+
+  while (left <= right) {
+    int mid = (left + right) / 2;
+    int target = array[mid];
+
+    if (target == num) {
+      return mid;
+    }
+
+    if (target > num) {
+      right = mid - 1;
+    }
+
+    else {
+      left = mid + 1;
+    }
+  }
+
+  return -1; // Return -1 if the num is not found
+}
+
+int main() {
+
+  int array[] = {1, 3, 5, 7, 9};
+  int array_length = sizeof(array) / sizeof(array[0]);
+
+  printf("Elemento esperado está na posição: %d\n", binarySearch(array, array_length, 5));
+  printf("%d\n", binarySearch(array, array_length, -1));
+  return 0;
+}
